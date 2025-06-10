c2c575ba85317b13632897cd8cf8846c45c8db87
yash garg <yg292001@gmail.com>
2025-06-10 15:21:23 +0530

    added copy to clipboard option

--- frontend/src/components/RoomStatus.jsx
+++ frontend/src/components/RoomStatus.jsx
@@ -1,11 +1,15 @@
 import { useUserStore } from "../store/useUserStore";
-
+import {Clipboard} from "lucide-react";
+import toast from "react-hot-toast";
 export function RoomStatus() {
     const {roomId, users} = useUserStore();
     return <div className='flex-0  w-9/10 max-w-5xl'>
       <div className='flex flex-wrap justify-between'>
         <kbd className="kbd kbd-lg">Connected users : {Object.keys(users).length}</kbd>
-        <kbd className="kbd kbd-lg">Invite code : {roomId}</kbd>
+        <kbd className="kbd kbd-lg flex gap-1 tooltip" data-tip="click to copy" onClick={()=>{
+          navigator.clipboard.writeText(roomId);
+          toast.success("Copied to clipboard",{duration: 2000});
+        }}>Invite code : {roomId} <Clipboard size={22}/></kbd>
       </div>
     </div>
   }
